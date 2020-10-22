c64b13510a1b1e5e5df80f2de53a0fbd6e68ea2b
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-22 14:32:38 -0600

    added the post, and the get one routes for the categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,10 +11,30 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: { id: req.params.id },
+    attributes: ['id', 'category_name']
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category with this id' });
+        return;
+      }
+
+      res.json(dbCategoryData);
+    })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
