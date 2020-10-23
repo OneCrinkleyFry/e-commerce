4a3d44615bfa0d00e299b2fc87d7ca8add168099
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-23 12:48:47 -0600

    added the product association to tags routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,10 @@ router.get('/', (req, res) => {
   // find all tags
   // includes its associated Product data
   Tag.findAll({
-    attributes: ['id', 'tag_name']
+    attributes: ['id', 'tag_name'],
+    include: {
+      model: Product
+    }
   })
     .then(dbTagData => res.json(dbTagData))
     .catch(err => {
@@ -20,7 +23,10 @@ router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // includes its associated Product data
   Tag.findOne({
-    attributes: ['id', 'tag_name']
+    attributes: ['id', 'tag_name'],
+    include: {
+      model: Product
+    }
   })
     .then(dbTagData => {
       if (!dbTagData) {
